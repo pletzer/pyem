744243c6b351da43c7930da82a9a6cb284a84a6e
Alex Pletzer <alexander@gokliya.net>
2021-02-03 06:50:53 +1300

    got trees to work

---
+++ trees.py
@@ -0,0 +1,14 @@
+import pyem
+import numpy as np
+
+data = np.array(np.loadtxt('data/binary.txt'), np.float64)
+
+G = 2
+res = pyem.cluster_bernoulli(data, G=G, maxiter=1000, max_diff=1.e-10, seed=123)
+
+print(f"""
+Number of groups: {G}
+theta: {res['theta_gj']}
+p_ig: {res['p_ig']}
+pi_g: {res['pi_g']}
+	""")
